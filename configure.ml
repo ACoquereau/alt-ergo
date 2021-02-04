d6bb6aeeb16bf6a641c8817d3c1b25f8419eda50
Albin Coquereau <6535385+ACoquereau@users.noreply.github.com>
2021-02-04 11:30:48 +0100

    Add rule static ubuntu

--- configure.ml
+++ configure.ml
@@ -148,8 +148,15 @@ let () =
   let () = Format.printf "Generating file %s..." f in
   let ch = open_out f in
   let fmt = Format.formatter_of_out_channel ch in
-  let () = Format.fprintf fmt {|(-linkall %s)@.|}
-      (if !static then "-cclib -static" else "")
+  let () = Format.fprintf fmt {|  %s@.|}
+      (if !static then
+         "( -cclib -Wl,-Bstatic \
+          -cclib -lunix \
+          -cclib -lz \
+          -cclib -lgmp \
+          -cclib -Wl,-Bdynamic \
+          -cclib -static-libgcc)"
+       else "(-linkall)")
   in
   let () = close_out ch in
   let () = Format.printf "done.@." in
